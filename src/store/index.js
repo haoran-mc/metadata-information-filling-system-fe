24d5f1f91c2bbec275da7d54d7db1437ddaa9d7e
haoran <haoran.mc@outlook.com>
2021-12-22 23:18:54 +0800

    :sparkles:存储后端传来的的 token，以及用户信息

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: localStorage.getItem('token'),
+    userInfo: JSON.parse(sessionStorage.getItem('userInfo')),
+    key: Date.now()
   },
   mutations: {
+    // set，状态与载体
+    SET_TOKEN: (state, token) => {
+      state.token = token
+      localStorage.setItem('token', token)
+    },
+    SET_USERINFO: (state, userInfo) => {
+      state.userInfo = userInfo
+      // sessionStorage 只能存字符串
+      sessionStorage.setItem('userInfo', JSON.stringify(userInfo))
+    },
+    REMOVE_INFO: (state) => {
+      state.token = ''
+      state.userInfo = {}
+      // localStorage 与 sessionStorage
+      localStorage.setItem('token', '')
+      sessionStorage.setItem('userInfo', JSON.stringify(''))
+    },
+    increment (state) {
+      state.key = Date.now()
+    }
+  },
+  getters: {
+    // get，也可以直接通过 state.token 来获得
+    getToken: state => {
+      return state.token
+    },
+    getUser: state => {
+      return state.userInfo
+    }
   },
   actions: {
   },
