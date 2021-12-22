fc6971685b6120da33a91ee7c4ca70836df5b3e0
haoran <haoran.mc@outlook.com>
2021-12-22 23:19:50 +0800

    :lipstick:设置全局的样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './axios'
+
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
