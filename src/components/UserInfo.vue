3599e057e98f97a24e829ad90270c9c2aef4fe7c
Woft6c <768276454@qq.com>
2021-12-27 22:10:31 +0800

    :lipstick:userinfo修改样式

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -6,7 +6,7 @@
       ref="loginFormRef"
       label-width="80px" label-position = "left">
       <!-- 姓名 -->
-      <el-form-item prop="username" label = "姓名">
+      <el-form-item prop="username" label = "姓名" style="width: 50%">
         <el-input v-model="UserForm.username"></el-input>
       </el-form-item>
 
@@ -17,46 +17,45 @@
 
       <!-- 联系方式 -->
       <el-form-item prop="phone" label = "联系方式">
-        <el-input v-model="UserForm.phone" disabled></el-input>
+        <el-input v-model="UserForm.phone" disabled  style="width: 50%"></el-input>
       </el-form-item>
 
       <!-- 密码 -->
       <el-form-item prop="password" label = "密码">
-        <el-input v-model="UserForm.password" disabled type="password"></el-input>&nbsp;&nbsp;
-        <el-button @click="dialogFormVisible = true">修改</el-button>
-
-        <!-- 密码修改弹窗 -->
-        <el-dialog title="修改密码" :visible.sync="dialogFormVisible" >
-          <el-form :model="ruleForm"
-                   status-icon :rules="rules"
-                   ref="ruleForm"
-                   label-width="100px"
-                   class="demo-ruleForm">
-            <el-form-item label="密码" prop="pass">
-              <el-input type="password" v-model="ruleForm.pass" autocomplete="off" placeholder="请输入密码"></el-input>
-            </el-form-item>
-            <br>
-            <el-form-item label="确认密码" prop="checkPass">
-              <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-            </el-form-item>
-            <br>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>&nbsp;&nbsp;
-              <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-          </el-form>
-          <div slot="footer" class="dialog-footer">
-            <el-button @click="dialogFormVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-          </div>
-        </el-dialog>
+        <el-input v-model="UserForm.password" disabled type="password" style="width: 50%"></el-input>
+        <el-button @click="dialogFormVisible = true" style="margin-left: 5%">修改</el-button>
       </el-form-item>
       <!-- 按钮 -->
-      <el-form-item>
-        <el-button>取消</el-button>&nbsp;&nbsp;
-        <el-button type="primary" @click="userFormPut">保存</el-button>
+      <el-form-item>&nbsp;&nbsp;
+        <el-button type="primary" @click="userFormPut" style="margin-left: -2%">保存</el-button>
       </el-form-item>
     </el-form>
+
+      <!-- 密码修改弹窗 -->
+      <el-dialog title="修改密码" :visible.sync="dialogFormVisible" >
+        <el-form :model="ruleForm"
+                 status-icon :rules="rules"
+                 ref="ruleForm"
+                 label-width="100px"
+                 class="demo-ruleForm">
+          <el-form-item label="输入密码" prop="pass">
+            <el-input type="password" v-model="ruleForm.pass" autocomplete="off" placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <br>
+          <el-form-item label="确认密码" prop="checkPass">
+            <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off" placeholder="请再次输入密码"></el-input>
+          </el-form-item>
+          <br>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>&nbsp;&nbsp;
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
+      </el-dialog>
     </el-card>
   </el-container>
 </template>
