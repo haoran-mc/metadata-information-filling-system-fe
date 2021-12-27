9931320883a712fcf42c511a0d567089262ddafb
Woft6c <768276454@qq.com>
2021-12-27 23:48:33 +0800

    :tada:无线滚动新增插件

---
+++ src/plugins/infinite-scroll.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+
+import infiniteScroll from 'vue-infinite-scroll'
+import elTableInfiniteScroll from 'el-table-infinite-scroll'
+
+Vue.use(
+  infiniteScroll,
+  elTableInfiniteScroll
+)
+
+// 单独的组件里面使用
+// import infiniteScroll from 'vue-infinite-scroll'
+// new Vue({
+//   directives: {infiniteScroll}
+// }
