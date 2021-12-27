4fb7c50d25d4f973a985aada94316e7d1c8478b1
Woft6c <768276454@qq.com>
2021-12-28 02:13:28 +0800

    :sparkles:添加插件

--- src/plugins/infinite-scroll.js
+++ src/plugins/infinite-scroll.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 
-import infiniteScroll from 'vue-infinite-scroll'
+// import infiniteScroll from 'vue-infinite-scroll'
 import elTableInfiniteScroll from 'el-table-infinite-scroll'
 
-Vue.use(
-  infiniteScroll,
-  elTableInfiniteScroll
-)
+Vue.use(elTableInfiniteScroll)
 
 // 单独的组件里面使用
 // import infiniteScroll from 'vue-infinite-scroll'
