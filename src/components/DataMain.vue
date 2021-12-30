21eb8fa0d0fa13e7c6fbe703ee244cb4576c4a9e
haoran <haoran.mc@outlook.com>
2021-12-30 21:06:46 +0800

    :sparkles:数据页编辑与详情按钮

--- src/components/DataMain.vue
+++ src/components/DataMain.vue
@@ -42,115 +42,120 @@
     <el-dialog
     :visible.sync="editorProjectDialogVisible"
     width="30%"
-    @close="editorProjectDialogClosed">
+    >
     <el-form
-      :model="editorProjectForm"
+      :inline="true"
+      :model="detailProjectForm"
       ref="editorProjectFormRef"
       title="编辑"
       >
-      <div class="data-project">
-        <el-form-item prop="name" label="项目名">
-          <el-input v-model="editorProjectForm.project.name"></el-input>
+      <div class="card">
+        <el-form-item prop="projectName" label="项目名">
+          <el-input v-model="detailProjectForm.name"></el-input>
         </el-form-item>
         <el-form-item prop="host" label="主持人">
-          <el-input v-model="editorProjectForm.project.host"></el-input>
+          <el-input v-model="detailProjectForm.host"></el-input>
         </el-form-item>
         <el-form-item prop="category" label="项目性质">
-          <el-input v-model="cascaderValue"></el-input>
+          <el-input v-model="detailProjectForm.cascader"></el-input>
         </el-form-item>
         <el-form-item prop="department" label="部门">
-          <el-input v-model="editorProjectForm.project.department"></el-input>
+          <el-input v-model="detailProjectForm.department"></el-input>
         </el-form-item>
         <el-form-item prop="money" label="经费">
-          <el-input v-model="editorProjectForm.project.money"></el-input>
+          <el-input v-model="detailProjectForm.money"></el-input>
         </el-form-item>
         <el-form-item prop="number" label="项目编号">
-          <el-input v-model="editorProjectForm.project.number"></el-input>
+          <el-input v-model="detailProjectForm.number"></el-input>
         </el-form-item>
         <el-form-item prop="start_time" label="项目开始时间">
-          <el-input v-model="editorProjectForm.project.start_time"></el-input>
+          <el-input v-model="detailProjectForm.start_time"></el-input>
         </el-form-item>
         <el-form-item prop="end_time" label="项目结束时间">
-          <el-input v-model="editorProjectForm.project.end_time"></el-input>
+          <el-input v-model="detailProjectForm.end_time"></el-input>
         </el-form-item>
-        <el-form-item prop="attachment" label="附件">
-          <el-input v-model="editorProjectForm.project.attachment"></el-input>
+        <el-form-item prop="attachment_path" label="附件">
+          <el-input v-model="detailProjectForm.attachment_path"></el-input>
         </el-form-item>
       </div>
+      <div>
+        <el-button type="primary" style="margin-left: 80%">确定</el-button>
+      </div>
     </el-form>
-  </el-dialog>
+    </el-dialog>
     <!-- 编辑对话框-教材 -->
     <el-dialog
       :visible.sync="editorTextbookDialogVisible"
       width="30%"
-      @close="editorTextbookDialogClosed">
+    >
       <el-form
-        :model="editorTextbookForm"
+        :inline="true"
+        :model="detailTextbookForm"
         ref="editorTextbookFormRef"
         title="编辑"
       >
-        <div class="data-project">
-          <el-form-item prop="name" label="教材名">
-            <el-input v-model="editorTextbookForm.textbook.name"></el-input>
+        <div class="card">
+          <el-form-item prop="textbookName" label="教材名">
+            <el-input v-model="detailTextbookForm.name"></el-input>
           </el-form-item>
           <el-form-item prop="host" label="主编">
-            <el-input v-model="editorTextbookForm.textbook.host"></el-input>
+            <el-input v-model="detailTextbookForm.editor"></el-input>
           </el-form-item>
-          <el-form-item prop="username" label="填表人">
-            <el-input v-model="editorTextbookForm.textbook.username"></el-input>
+          <el-form-item prop="userPhone" label="填表人">
+            <el-input v-model="detailTextbookForm.phone"></el-input>
           </el-form-item>
-          <el-form-item prop="publishhouse" label="出版社">
-            <el-input v-model="editorTextbookForm.textbook.publishing_house"></el-input>
+          <el-form-item prop="publishHouse" label="出版社">
+            <el-input v-model="detailTextbookForm.publishing_house"></el-input>
           </el-form-item>
-          <el-form-item prop="publishing_time" label="出版时间">
-            <el-input v-model="editorTextbookForm.textbook.publishing_time"></el-input>
+          <el-form-item prop="publishingTime" label="出版时间">
+            <el-input v-model="detailTextbookForm.publishing_time"></el-input>
           </el-form-item>
-          <el-form-item prop="selection_time" label="发行时间">
-            <el-input v-model="editorTextbookForm.textbook.selection_time"></el-input>
+          <el-form-item prop="selectionTime" label="发行时间">
+            <el-input v-model="detailTextbookForm.selection_time"></el-input>
           </el-form-item>
-          <el-form-item prop="attachment" label="附件">
-            <el-input v-model="editorProjectForm.project.attachment"></el-input>
+          <el-form-item prop="attachment_path" label="附件">
+            <el-input v-model="detailProjectForm.attachment_path"></el-input>
           </el-form-item>
+        </div>
+        <div>
+          <el-button type="primary" style="margin-left: 80%">确定</el-button>
         </div>
       </el-form>
     </el-dialog>
     <!-- 详细对话框-项目 -->
     <el-dialog
       :visible.sync="detailProjectDialogVisible"
-      width="30%"
-      @close="detailProjectDialogClosed">
+      width="30%">
       <el-form
+        :inline="true"
         :model="detailProjectForm"
         ref="detailProjectFormRef"
         title="详细"
       >
-        <div class="data-project">
-          <el-form-item prop="name" label="项目名">
-            <el-input v-model="detailProjectForm.project.name"></el-input>
+        <div class="card">
+          <el-form-item prop="projectName" label="项目名">
+            <el-input disabled v-model="detailProjectForm.name"></el-input>
           </el-form-item>
           <el-form-item prop="host" label="主持人">
-            <el-input v-model="detailProjectForm.project.host"></el-input>
+            <el-input disabled v-model="detailProjectForm.host"></el-input>
           </el-form-item>
           <el-form-item prop="category" label="项目性质">
-            <el-input v-model="cascaderValue"></el-input>
+            <el-input disabled v-model="detailProjectForm.cascader"></el-input>
           </el-form-item>
           <el-form-item prop="department" label="部门">
-            <el-input v-model="detailProjectForm.project.department"></el-input>
+            <el-input disabled v-model="detailProjectForm.department"></el-input>
           </el-form-item>
           <el-form-item prop="money" label="经费">
-            <el-input v-model="detailProjectForm.project.money"></el-input>
+            <el-input disabled v-model="detailProjectForm.money"></el-input>
           </el-form-item>
           <el-form-item prop="number" label="项目编号">
-            <el-input v-model="detailProjectForm.project.number"></el-input>
+            <el-input disabled v-model="detailProjectForm.number"></el-input>
           </el-form-item>
           <el-form-item prop="start_time" label="项目开始时间">
-            <el-input v-model="detailProjectForm.project.start_time"></el-input>
+            <el-input disabled v-model="detailProjectForm.start_time"></el-input>
           </el-form-item>
           <el-form-item prop="end_time" label="项目结束时间">
-            <el-input v-model="detailProjectForm.project.end_time"></el-input>
-          </el-form-item>
-          <el-form-item prop="attachment" label="附件">
-            <el-input v-model="detailProjectForm.project.attachment"></el-input>
+            <el-input disabled v-model="detailProjectForm.end_time"></el-input>
           </el-form-item>
         </div>
       </el-form>
@@ -158,34 +163,31 @@
     <!-- 详细对话框-教材 -->
     <el-dialog
       :visible.sync="detailTextbookDialogVisible"
-      width="30%"
-      @close="detailTextbookDialogClosed">
+      width="30%">
       <el-form
+        :inline="true"
         :model="detailTextbookForm"
         ref="detailTextbookFormRef"
         title="详细"
       >
-        <div class="data-project">
-          <el-form-item prop="name" label="教材名">
-            <el-input v-model="detailTextbookForm.textbook.name"></el-input>
+        <div class="card">
+          <el-form-item prop="textbookName" label="教材名">
+            <el-input disabled v-model="detailTextbookForm.name"></el-input>
           </el-form-item>
-          <el-form-item prop="host" label="主编">
-            <el-input v-model="detailTextbookForm.textbook.host"></el-input>
+          <el-form-item prop="editor" label="主编">
+            <el-input disabled v-model="detailTextbookForm.editor"></el-input>
           </el-form-item>
-          <el-form-item prop="username" label="填表人">
-            <el-input v-model="detailTextbookForm.textbook.username"></el-input>
+          <el-form-item prop="userPhone" label="填表人电话">
+            <el-input disabled v-model="detailTextbookForm.phone"></el-input>
           </el-form-item>
-          <el-form-item prop="publishhouse" label="出版社">
-            <el-input v-model="detailTextbookForm.textbook.publishing_house"></el-input>
+          <el-form-item prop="publish_house" label="出版社">
+            <el-input disabled v-model="detailTextbookForm.publishing_house"></el-input>
           </el-form-item>
           <el-form-item prop="publishing_time" label="出版时间">
-            <el-input v-model="detailTextbookForm.textbook.publishing_time"></el-input>
+            <el-input disabled v-model="detailTextbookForm.publishing_time"></el-input>
           </el-form-item>
           <el-form-item prop="selection_time" label="发行时间">
-            <el-input v-model="detailTextbookForm.textbook.selection_time"></el-input>
-          </el-form-item>
-          <el-form-item prop="attachment" label="附件">
-            <el-input v-model="detailProjectForm.project.attachment"></el-input>
+            <el-input disabled v-model="detailTextbookForm.selection_time"></el-input>
           </el-form-item>
         </div>
       </el-form>
@@ -261,7 +263,7 @@ export default {
           number: '',
           start_time: '',
           end_time: '',
-          attachment: '',
+          attachment_path: '',
           year: '',
           batch: ''
         }
@@ -305,14 +307,24 @@ export default {
           _this.$message.error('获取数据失败')
           return res
         }
-        console.log(res)
-        this.tableData = res.data.data
+        _this.tableData = res.data.data
+
+        if (_this.category === 'project') {
+          for (const item in _this.tableData) {
+            console.log(item)
+            _this.tableData[item].cascader = _this.tableData[item].category_first_level + '/' + _this.tableData[item].category_second_level + '/' + _this.tableData[item].category_third_level
+          }
+        }
+
+        console.log('--', _this.tableData)
       })
     },
+    // 分页按钮
     handleCurrentChange (newPage) {
       this.page_num = newPage
       this.getDataBySessionInfo()
     },
+    // 面包屑中文
     changeCategoryEnglishToChinese () {
       if (this.category === 'project') {
         this.category_chinese = '项目'
@@ -320,6 +332,7 @@ export default {
         this.category_chinese = '教材'
       }
     },
+    // 编辑按钮
     handleEdit (index, row) {
       if (this.category === 'project') {
         this.editorProjectDialogVisible = true
@@ -328,26 +341,50 @@ export default {
       }
       console.log(index, row)
     },
+    // 删除按钮
+    handleDelete (index, row) {
+      const _this = this
+
+      if (this.category === 'project') {
+        this.$axios.delete('/projects', {
+          params: {
+            id: row.id
+          }
+        }).then(res => {
+          if (res.data.code !== 200) {
+            _this.$message.error('删除失败')
+          }
+          _this.$message.success('删除成功')
+        })
+      } else {
+        this.$axios.delete('/textbooks').then(res => {
+          if (res.data.code !== 200) {
+            _this.$message.error('删除失败')
+          }
+          _this.$message.success('删除成功')
+        })
+      }
+      this.$router.go(0)
+    },
+    // 导出按钮
+    handleOut (index, row) {
+      console.log(index, row)
+    },
+    // 详情按钮
+    handleCareful (index, row) {
+      if (this.category === 'project') {
+        this.detailProjectDialogVisible = true
+        this.detailProjectForm = row
+      } else {
+        this.detailTextbookDialogVisible = true
+        this.detailTextbookForm = row
+      }
+    },
     editorProjectDialogClosed () {
       this.$refs.editorProjectFormRef.resetFields()
     },
     editorTextbookDialogClosed () {
       this.$refs.editorTextbookFormRef.resetFields()
-    },
-    detailProjectDialogClosed () {
-      this.$refs.detailProjectFormRef.resetFields()
-    },
-    detailTextbookDialogClosed () {
-      this.$refs.detailTextbookFormRef.resetFields()
-    },
-    handleDelete (index, row) {
-      console.log(index, row)
-    },
-    handleOut (index, row) {
-      console.log(index, row)
-    },
-    handleCareful (index, row) {
-      console.log(index, row)
     }
   }
 }
@@ -358,5 +395,12 @@ export default {
   margin-right: 20px;   /* 因为 el_card__body padding = 20px */
   margin-top: 58px;   /* 与 aside 一样，上边距为 58px */
   width: 100%;   /* 尽量不用具体数据 */
+}
+
+.card {
+  padding: 20px;
+  background-color: #F6F8F8;
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 </style>
